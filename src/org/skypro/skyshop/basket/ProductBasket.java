8dce924e8abcaf1c07dbf3bb145f6e58b0590066
ArtemGRB <artemgrb@gmail.com>
2025-03-11 23:34:31 +0700

    HW17

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -4,27 +4,25 @@ import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ProductBasket {
-    private Product[] productBasket = new Product[5];
+    private List<Product> productBasket = new ArrayList<>();
 
     //Добавление товара в корзину
     public void addToBasket(Product product) {
-        for (int i = 0; i < productBasket.length; i++) {
-            if (productBasket[i] == null) {
-                productBasket[i] = product;
-                return;
-            }
-        }
-        System.out.println("Невозможно добавить продукт");
+        productBasket.add(product);
     }
 
     //Метод получения общей стоимости корзины: метод ничего не принимает и возвращает целое число.
     public int getSumOfProducts() {
         int sum = 0;
-        for (int i = 0; i < productBasket.length; i++) {
-            if (productBasket[i] != null) {
-                sum += productBasket[i].getPrice();
+        for (int i = 0; i < productBasket.size(); i++) {
+            if (productBasket.get(i) != null) {
+                sum += productBasket.get(i).getPrice();
             }
         }
         return sum;
@@ -32,9 +30,9 @@ public class ProductBasket {
 
     //Метод печатает содержимое корзины
     public void printProductsOfBasket() {
-        for (int i = 0; i < productBasket.length; i++) {
-            if (productBasket[i] != null) {
-                System.out.println(productBasket[i]);
+        for (int i = 0; i < productBasket.size(); i++) {
+            if (productBasket.get(i) != null) {
+                System.out.println(productBasket.get(i));
             }
         }
         if (getSumOfProducts() != 0) {
@@ -48,8 +46,8 @@ public class ProductBasket {
 
     //проверка наличия товара в корзине по названию
     public boolean checkProductInBasket(String nameOfProduct) {
-        for (int i = 0; i < productBasket.length; i++) {
-            if (productBasket[i] != null && productBasket[i].getName().equals(nameOfProduct)) {
+        for (int i = 0; i < productBasket.size(); i++) {
+            if (productBasket.get(i).getName().equals(nameOfProduct)) {
                 return true;
             }
         }
@@ -58,20 +56,35 @@ public class ProductBasket {
 
     //Очистка корзины
     public void cleanBasket() {
-        for (int i = 0; i < productBasket.length; i++) {
-            productBasket[i] = null;
+        for (int i = 0; i < productBasket.size(); i++) {
+            productBasket.remove(i);
         }
     }
 
     //подсчет количество специальных товаров
     public int getCountSpecialProduct() {
         int count = 0;
-        for (int i = 0; i < productBasket.length; i++) {
-            if (productBasket[i] != null && productBasket[i].isSpecial()) {
+        for (int i = 0; i < productBasket.size(); i++) {
+            if (productBasket.get(i) != null && productBasket.get(i).isSpecial()) {
                 count++;
             }
         }
         return count;
+    }
+
+    //удаления продукта по имени из корзины
+    public List<Product> removeFromBasketByName(String name) {
+        List<Product> result = new ArrayList<>();
+        Iterator<Product> iterator = productBasket.iterator();
+
+        while (iterator.hasNext()) {
+            Product element = iterator.next();
+            if(element.getName().equals(name)){
+                result.add(element);
+                iterator.remove();
+            }
+        }
+        return result;
     }
 
 }
--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -2,64 +2,53 @@ package org.skypro.skyshop.search;
 
 import org.skypro.skyshop.exeption.BestResultNotFound;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchEngine {
 
-    private Searchable[] searchables;
+    private List<Searchable> searchables = new ArrayList<>();
     private int currentIndex = 0;
 
-    public SearchEngine(int capacity) {
-        this.searchables = new Searchable[capacity];
-    }
 
     public void add(Searchable searchable) {
-        if (currentIndex < searchables.length) {
-            searchables[currentIndex] = searchable;
-            currentIndex++;
-        } else {
-            System.out.println("Массив заполнен");
-        }
+        searchables.add(searchable);
     }
 
+    // поиск в Searchable объектах по строке
+    public List<Searchable> search(String query) {
 
-    public Searchable[] search(String query) {
-        Searchable[] results = new Searchable[5];
-        int count = 0;
-
+        List<Searchable> results = new ArrayList<>();
 
         for (Searchable searchable : searchables) {
 
-            if (searchable != null && searchable.getSearchTerm().contains(query)) {
-                results[count] = searchable; // Добавляем в результаты
-                count++;
-
-                if (count == 5) {
-                    break;
-                }
+            if (searchable.getSearchTerm().contains(query)) {
+                results.add(searchable); // Добавляем в результаты
             }
         }
-
         return results;
     }
 
+
+    // поиск Searchable объекта в котором больше всего встречается поисковая строка
     public Searchable search2(String substring) {
         int count = 0;
         Searchable result = null;
         for (Searchable a : searchables) {
-            if (a != null) {
-                String str = a.getSearchTerm();
-                int quantity = 0;
-                int index = 0;
-                int indexString = str.indexOf(substring, index);
 
-                while (indexString != -1) {
-                    quantity++;
-                    index = indexString + substring.length();
-                    indexString = str.indexOf(substring, index);
-                }
-                if (quantity > count) {
-                    count = quantity;
-                    result = a;
-                }
+            String str = a.getSearchTerm();
+            int quantity = 0;
+            int index = 0;
+            int indexString = str.indexOf(substring, index);
+
+            while (indexString != -1) {
+                quantity++;
+                index = indexString + substring.length();
+                indexString = str.indexOf(substring, index);
+            }
+            if (quantity > count) {
+                count = quantity;
+                result = a;
             }
         }
 
