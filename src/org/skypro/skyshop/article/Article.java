7c3fad40e6c0d03d47e714c1a1f352b7dd6e1509
ArtemGRB <artemgrb@gmail.com>
2025-03-15 11:15:57 +0700

    HW18

--- src/org/skypro/skyshop/article/Article.java
+++ src/org/skypro/skyshop/article/Article.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.article;
 
 import org.skypro.skyshop.search.Searchable;
 
+import java.util.Objects;
+
 public final class Article implements Searchable {
     private String title;
     private String article;
@@ -9,6 +11,18 @@ public final class Article implements Searchable {
     public Article(String title, String article) {
         this.title = title;
         this.article = article;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(title);
     }
 
     public String toString() {
